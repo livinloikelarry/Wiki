5ecf39bddc35d2db66aa0e969e64757ea9e8d157
Yuvia Leon <ykl2112@columbia.edu>
2023-08-05 10:55:06 -0500

    match in search bar redirects user to entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django import forms
 from . import util
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 class SearchEncyclopediaForm(forms.Form):
@@ -9,6 +11,22 @@ class SearchEncyclopediaForm(forms.Form):
 
 
 def index(request):
+    if request.method == "POST":
+        form = SearchEncyclopediaForm(request.POST)
+        if form.is_valid():
+            # get the user input and see if it is an existing entry
+            term = form.cleaned_data["term"]
+            options = [term, term.upper(), term.capitalize(), term.lower()]
+            for o in options:
+                if o in util.list_entries():
+                    term = o
+                    return HttpResponseRedirect(reverse("EntryPage", args=[term]))
+        else:  # form not valid
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries(),
+                "form": form
+            })
+    # else if method is GET
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
         "form": SearchEncyclopediaForm()
